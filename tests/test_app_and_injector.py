66bb4aaf6fd745bece4ad7fffa135a78fb693acf
Martin Atkins <mart@degeneration.co.uk>
2014-03-29 19:01:51 -0700

    Basic application and injector functionality.
    This includes the interface for declaring dependencies but so far lacks
    a convenient way to define interfaces and providers.

---
+++ tests/test_app_and_injector.py
@@ -0,0 +1,208 @@
+
+import unittest
+from mock import MagicMock
+from tiedye import Application, Injector
+
+
+class TestAppAndInjector(unittest.TestCase):
+
+    def test_separate_dependency_registration(self):
+        def dummy():
+            pass
+
+        interface = MagicMock(name="baz_interface")
+
+        app = Application()
+        app.dependencies(dummy, baz=interface)
+
+        self.assertEqual(
+            dict(app.dependency_map),
+            {
+                dummy: {
+                    "baz": interface,
+                }
+            }
+        )
+
+        # and dummy can still be called
+        dummy()
+
+    def test_decorator_dependency_registration(self):
+        interface = MagicMock(name="baz_interface")
+        app = Application()
+
+        @app.dependencies(baz=interface)
+        def dummy():
+            pass
+
+        self.assertEqual(
+            dict(app.dependency_map),
+            {
+                dummy: {
+                    "baz": interface,
+                }
+            }
+        )
+
+        # and dummy can still be called
+        dummy()
+
+    def test_invalid_dependency_registration(self):
+        app = Application()
+
+        def dummy():
+            pass
+
+        def dummy2():
+            pass
+
+        with self.assertRaises(TypeError):
+            # invalid because there are two positional arguments
+            # when only zero or one is permitted
+            app.dependencies(dummy, dummy2, baz=dummy)
+
+    def test_make_injector(self):
+        app = Application()
+        interface1 = MagicMock(name="interface1")
+        interface2 = MagicMock(name="interface2")
+        interface3 = MagicMock(name="interface3")
+
+        def provider1(interface, injector):
+            pass
+
+        def provider2(interface, injector):
+            pass
+
+        injector = app.make_injector({
+            interface1: provider1,
+            interface2: provider1,
+        })
+        specialized_injector = injector.specialize({
+            interface2: provider2,
+            interface3: provider2,
+        })
+
+        self.assertEqual(
+            type(injector),
+            Injector,
+        )
+        self.assertEqual(
+            injector.app,
+            app,
+        )
+        # Our given providers are registered.
+        self.assertEqual(
+            injector.providers[interface1],
+            provider1,
+        )
+        self.assertEqual(
+            injector.providers[interface2],
+            provider1,
+        )
+        # Injector registers a provider for itself.
+        self.assertEqual(
+            injector.providers[Injector](Injector),
+            injector,
+        )
+        # No other providers are registered.
+        self.assertEqual(
+            len(injector.providers),
+            3,
+        )
+
+        self.assertEqual(
+            type(specialized_injector),
+            Injector,
+        )
+        self.assertEqual(
+            specialized_injector.app,
+            app,
+        )
+        self.assertEqual(
+            specialized_injector.providers[interface1],
+            provider1,
+        )
+        self.assertEqual(
+            specialized_injector.providers[interface2],
+            provider2,
+        )
+        self.assertEqual(
+            specialized_injector.providers[interface3],
+            provider2,
+        )
+        self.assertEqual(
+            specialized_injector.providers[Injector](Injector),
+            specialized_injector,
+        )
+        self.assertEqual(
+            len(specialized_injector.providers),
+            4,
+        )
+
+    def test_injector_full_bind(self):
+        app = Application()
+        interface1 = MagicMock(name="interface1")
+        # Using a tuple as in interface is not a normal case but it works
+        # well enough for this test, and shows that we aren't doing anything
+        # that *prevents* using a tuple as an interface, if a caller wants to
+        # do something unusual.
+        interface2 = ("hi","world")
+        interface_type = type(interface2)
+
+        # the provider itself gets bound, so it can declare dependencies
+        # if it wants to.
+        @app.dependencies(inj=Injector)
+        def interface1_provider(iface, inj):
+            return "interface1"
+
+        injector = app.make_injector({
+            # provider for a specific interface instance
+            interface1: interface1_provider,
+            # provider for a whole type of interface, which is used
+            # to support cases like automatic RPC proxy generation, etc.
+            interface_type: lambda iface: "type %s" % iface[0]
+        })
+
+        @app.dependencies(a=interface1, b=interface2)
+        def func(a, b):
+            return (a, b)
+
+        bound_func = injector.bind(func)
+
+        self.assertTrue(
+            callable(bound_func),
+        )
+        self.assertEqual(
+            bound_func(),
+            ("interface1", "type hi"),
+        )
+
+    def test_injector_partial_bind(self):
+        app = Application()
+        interface1 = MagicMock(name="interface1")
+        interface2 = MagicMock(name="interface1")
+
+        @app.dependencies(a=interface1, b=interface2)
+        def func(a, b):
+            return (a, b)
+
+        injector1 = app.make_injector({
+            interface1: lambda iface: "hi",
+        })
+        partial_bound_func = injector1.bind(func)
+
+        self.assertEqual(
+            partial_bound_func(b="world"),
+            ("hi", "world"),
+        )
+
+        injector2 = injector1.specialize({
+            interface2: lambda iface: "cheese",
+        })
+
+        bound_func = injector2.bind(partial_bound_func)
+
+        self.assertEqual(
+            bound_func(),
+            ("hi", "cheese"),
+        )
---
+++ tiedye/__init__.py
@@ -0,0 +1,155 @@
+
+import weakref
+import functools
+import types
+
+
+EMPTY_DEPS = {}
+
+
+class Application(object):
+
+    def __init__(self):
+        self.dependency_map = weakref.WeakKeyDictionary()
+
+    def dependencies(self, *args, **mapping):
+        def register(callable):
+            self.dependency_map[callable] = mapping
+            return callable
+        if len(args) == 1:
+            register(args[0])
+        elif len(args) == 0:
+            return register
+        else:
+            raise TypeError(
+                "Application.dependencies takes either one positional "
+                "argument (when used as a function) or no positional "
+                "arguments (when used as a decorator); %i given" % (
+                    len(args)
+                )
+            )
+
+    def make_injector(self, providers):
+        return Injector(self, providers)
+
+
+class Injector(object):
+    """
+    Implements a mapping from interfaces to object providers.
+
+    Once an application has dependency lists for a number of callables,
+    an injector from that application can be used to automatically provide
+    the dependency objects.
+
+    When instantiating an injector, the calling application passes in
+    a mapping object whose keys are either interface instances or interface
+    types and whose values are provider functions.
+
+    A provider function is any callable that takes an interface instance and
+    an injector as positional arguments and returns an implementation of the
+    given interface.
+    """
+
+    def __init__(self, app, providers):
+        self.app = app
+        self.providers = providers
+        self.currently_binding = set()
+        # Expose the injector itself as an injectable object
+        providers[Injector] = lambda dummy: self
+
+    def bind(self, func):
+        """
+        Bind dependencies to a callable.
+
+        Takes any callable and returns a new wrapper callable that
+        accepts the same arguments as the initial callable except that
+        registered dependencies are automatically passed in.
+
+        The returned callable can then be called with any further arguments
+        that were not provided as dependencies.
+
+        Any requested interfaces for which no provider is available will
+        be left unbound, and can thus be provided explicitly in a call
+        to the returned callable or bound separately by a later call to
+        :py:meth:`Injector.bind`; this is known as *partial binding*.
+        """
+
+        # If we've been given a bound method then we need to peel off
+        # the binding wrapper to find the item in our dependencies table,
+        # but we still want to return a wrapper around exactly what was
+        # provided, so an injected bound method stays bound.
+        if isinstance(func, types.MethodType):
+            lookup_func = func.im_func
+        else:
+            lookup_func = func
+
+        if lookup_func in self.currently_binding:
+            raise DependencyCycleError(
+                "Dependency cycle between the following callables: %s" % (
+                    ", ".join(self.currently_binding),
+                )
+            )
+
+        self.currently_binding.add(lookup_func)
+
+        try:
+            deps = self.app.dependency_map[lookup_func]
+        except KeyError:
+            deps = EMPTY_DEPS
+
+        kwargs = {}
+        unprovided = {}
+        for arg_name, interface in deps.iteritems():
+            interface_type = type(interface)
+            if interface in self.providers:
+                provider = self.providers[interface]
+            elif interface_type in self.providers:
+                provider = self.providers[interface_type]
+            else:
+                # Record unprovided interfaces to allow partial injection,
+                # e.g. to allow some interfaces to be injected at application
+                # startup time and then others to be injected on a
+                # per-request basis.
+                unprovided[arg_name] = interface
+                continue
+
+            # Recursively bind the provider too, so it can request
+            # dependencies of its own.
+            provider = self.bind(provider)
+
+            kwargs[arg_name] = provider(interface)
+
+        self.currently_binding.remove(lookup_func)
+
+        injected = functools.partial(func, **kwargs)
+        if len(unprovided) > 0:
+            self.app.dependencies(
+                injected,
+                **unprovided
+            )
+        return injected
+
+    def specialize(self, more_providers):
+        """
+        Create a new injector initialized with the same provider
+        configuration as this one, and for the same application,
+        but with some additional providers.
+
+        The primary use for this is to create an application-wide injector
+        on startup, containing items that should live for the lifetime of the
+        application, and then create a derived injector for each request
+        that includes additional request-specific items, such as an
+        object representing the requesting user.
+
+        This can be combined with partial binding (as described on
+        :py:meth:`Injector.bind`) to do up-front as much dependency
+        provision as possible, but defer a few request-specific dependencies
+        until the request is being handled.
+        """
+        new_providers = dict(self.providers)
+        new_providers.update(more_providers)
+        return Injector(self.app, new_providers)
+
+
+class DependencyCycleError(Exception):
+    pass
